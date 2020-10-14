7440cb65a56803be163ec7b8ddc52039a9e90618
Le Xuan Thang <lxthanghy@gmail.com>
2020-10-14 09:06:57 +0700

    cap nhat

--- src/app/main/contact/contact.module.ts
+++ src/app/main/contact/contact.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
-
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
