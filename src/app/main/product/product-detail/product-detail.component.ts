95a8ec227748349d5ba0fbe21f44adfaa19cec23
Le Xuan Thang <lxthanghy@gmail.com>
2020-10-21 15:32:26 +0700

    update cart

--- src/app/main/product/product-detail/product-detail.component.ts
+++ src/app/main/product/product-detail/product-detail.component.ts
@@ -2,30 +2,37 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { first, map, switchMap } from 'rxjs/operators';
 import { ProductService } from '../../../services/product.service';
+import { CartService } from '../../../services/cart.service';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent implements OnInit, AfterViewInit {
+export class ProductDetailComponent implements OnInit {
   product: any;
   imageUrl: string;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private readonly cartService: CartService
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap
       .pipe(
-        first(),
         map((params) => params.get('id')),
-        switchMap((id) => this.productService.GetById(id))
+        switchMap((id) => this.productService.GetById(id).pipe(first()))
       )
-      .subscribe((product) => {
-        this.product = product;
-        //console.log(this.product);
-      });
+      .subscribe((product) => (this.product = product));
   }
-  ngAfterViewInit(): void {}
+  addToCart(product: any, quantity: any) {
+    var pro = {
+      id: product.id,
+      imageUrl: product.imageUrl,
+      name: product.name,
+      price: product.price,
+      promotionPrice: product.promotionPrice,
+    };
+    this.cartService.addToCart(pro, parseInt(quantity));
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,8 +11,8 @@ export class CartService {
     if (!local_storage) local_storage = [];
     this.productsCart.next(local_storage);
   }
-  addToCart(product: any): void {
-    product.quantity = 1;
+  addToCart(product: any, quantity: number = 1): void {
+    product.quantity = quantity;
     let local_storage: any[] = [];
     if (localStorage.getItem('carts') == null) {
       local_storage.push(product);
@@ -22,7 +22,7 @@ export class CartService {
       for (let p of local_storage) {
         if (p.id == product.id) {
           checkProduct = false;
-          p.quantity += 1;
+          p.quantity += quantity;
           break;
         }
       }
